fd9f8cfd9dbb37ca95ca3fa7d1fd4a2cbea9170b
kerimdemir <kerimdemir@gmail>
2018-12-12 16:20:09 +0300

    develop commit

--- App.js
+++ App.js
@@ -20,7 +20,7 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-       <Text>redux example</Text>
+       <Text>develop example</Text>
       </View>
     );
   }
